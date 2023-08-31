1a3d89981971b3c9ecb0573294945a99a126335e
AlexeyDemidow <usernzt@gmail.com>
2023-08-31 20:56:26 +0300

    Solving mistakes

--- django_fit_calories_app/django_fit_calories_app/urls.py
+++ django_fit_calories_app/django_fit_calories_app/urls.py
@@ -25,7 +25,6 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-
     path('calories_calc/', include('calories_calc.urls')),
 ]
 
